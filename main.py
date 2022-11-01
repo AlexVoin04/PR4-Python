44e3e49cbb8c1d991271a56d1e418586d2f3d0e6
Alex <alexiv.04@mail.ru>
2022-11-01 10:45:33 +0700

    Third commit

--- main.py
+++ main.py
@@ -2,26 +2,30 @@ import multiprocessing
 from datetime import datetime
 
 
-# def get_input():
-#    while True:
-#        x, y = [int(x) for x in input().split()]
-#        if x == '' or y == '':
-#            break
-#        else:
-#            return x, y
-
-
-def get_numbers(queue):
-    # number = get_input()
-    try:
+def get_input():
+    while True:
         x, y = [int(x) for x in input().split()]
-        queue.put(x)
-        queue.put(y)
-    except Exception:
-        pass
+        if x == '' or y == '':
+           break
+        else:
+           return x, y
+
+
+def get_numbers(queue, x, y):
+    #number = get_input()
+    #x, y = [int(x) for x in input().split()]
+    queue.put(x)
+    queue.put(y)
+    #try:
+        #x, y = [int(x) for x in input().split()]
+        #queue.put(x)
+        #queue.put(y)
+    #except Exception:
+    #    pass
 
 
 def get_result(queue):
+
     try:
         while True:
             if not queue.empty():
@@ -34,8 +38,9 @@ def get_result(queue):
                 filename = r"./test.txt"
                 with open(filename, 'a') as file:
                     now = datetime.now()
-                    date_format = "{}.{}.{}  {}:{}:{}".format(now.day, now.month, now.year, now.hour, now.minute, now.second)
-                    file.write(f"{date_format} {x} {y} {step}")
+                    date_format = "{}.{}.{}  {}:{}:{}".format(now.day, now.month, now.year, now.hour, now.minute,
+                                                              now.second)
+                    file.write(f"{date_format} {x} {y} {step}\n")
     except Exception:
         print("Что-то пошло не так ...")
     except KeyboardInterrupt:
@@ -43,11 +48,14 @@ def get_result(queue):
 
 
 if __name__ == '__main__':
+
     queue = multiprocessing.Queue()
 
     try:
         while True:
-            firstProcess = multiprocessing.Process(target=get_numbers, args=(queue,))
+            x, y = [int(x) for x in input().split()]
+
+            firstProcess = multiprocessing.Process(target=get_numbers, args=(queue, x, y))
             secondProcess = multiprocessing.Process(target=get_result, args=(queue,))
             firstProcess.start()
             secondProcess.start()
@@ -56,5 +64,6 @@ if __name__ == '__main__':
             while not queue.empty():
                 pass
             print("Конец")
+
     except Exception:
-        pass
+        print("Что-то пошло не так ...")
